a9f3bb2a8c8bfc5a11cd247b748959d32c862f3f
svarga <steven@vargaconsulting.ca>
2025-03-25 10:50:53 -0400

    [#4]:svarga:pattern, example consumer

--- include/iex/consumers/example.hpp
+++ include/iex/consumers/example.hpp
@@ -16,5 +16,60 @@
 #ifndef IEX_EXAMPLE_CONSUMER_HPP
 #define	IEX_EXAMPLE_CONSUMER_HPP
 
+#include <string>
+#include <vector>
+#include <io/interface>
+#include <date/tz.h>
+#include <glog/logging.h>
+#include <unordered_map>
+
+using namespace std;
+using namespace date;
+
+namespace iex {
+	namespace ch = std::chrono;
+	template <class Clock> struct IOStreamConsumer :
+		public io::Consumer<IOStreamConsumer<Clock>, Clock> {
+		using time_point = typename Clock::time_point;
+		using duration = typename Clock::duration;
+
+		void begin(uint64_t I, uint64_t S,  const std::vector<duration>& rts ){};
+		void trade_report_impl(time_point time,  uint64_t stock, float price, uint64_t size, uint8_t flag );
+		void ask_impl(time_point time,  uint64_t stock, float price, uint64_t size, uint8_t flag );
+		void bid_impl(time_point time,  uint64_t stock, float price, uint64_t size, uint8_t flag );
+		void trade_break_impl(time_point time,  uint64_t stock, float price, uint64_t size, uint8_t flag );
+		void heart_beat_impl( time_point time );
+		void day_begin_impl( time_point day );
+		void day_end_impl( time_point day );
+
+		long count;
+		uint64_t seq;
+		std::unordered_map<uint64_t,uint64_t>map;
+	};
+}
+
+template <class Clock>
+void iex::IOStreamConsumer<Clock>::trade_report_impl(time_point time,  uint64_t stock, float price, uint64_t size, uint8_t flag ){
+}
+template <class Clock>
+void iex::IOStreamConsumer<Clock>::ask_impl(time_point time,  uint64_t stock, float price, uint64_t size, uint8_t flag ){
+}
+template <class Clock>
+void iex::IOStreamConsumer<Clock>::bid_impl(time_point time,  uint64_t stock, float price, uint64_t size, uint8_t flag ){
+}
+template <class Clock>
+void iex::IOStreamConsumer<Clock>::trade_break_impl(time_point time,  uint64_t stock, float price, uint64_t size, uint8_t flag ){
+}
+template <class Clock>
+void iex::IOStreamConsumer<Clock>::heart_beat_impl( time_point tp ){
+	using namespace std;
+	using namespace date;
+}
+template <class Clock>
+void iex::IOStreamConsumer<Clock>::day_begin_impl( time_point day ){
+}
+template <class Clock>
+void iex::IOStreamConsumer<Clock>::day_end_impl( time_point day ){
+}
 #endif
 
